a6316dded7519ed073b5190be545c6816834deca
hungryakira <jon.kh@hotmail.com>
2025-02-02 15:42:35 +0800

    Update main.py
    Added more comments

--- Flash Card/main.py
+++ Flash Card/main.py
@@ -8,7 +8,6 @@ current_card ={}
 word_choice = ""
 
 #-----------------------read csv--------------------#
-global word_dict
 
 #french_words is csv file with 2 columns
 
@@ -20,7 +19,7 @@ except FileNotFoundError:
     # if not available, create file
     french_df = pd.read_csv("data/french_words.csv")
     word_dict = french_df.to_dict(orient="records")
-    # print(word_dict)
+
 else:
     word_dict = french_df.to_dict(orient="records")  # french_df becomes active list
 
@@ -28,6 +27,7 @@ else:
 #[{'French': 'partie', 'English': 'part'}, {'French': 'histoire', 'English': 'history'}]
 
 #-----------------------functions--------------------#
+#New word is picked from the list word_dict. Update UI to show French word. After 4 sec, card is 'flipped'
 def pick_word():
     global current_card, timer, word_dict
     window.after_cancel(timer)
@@ -36,9 +36,11 @@ def pick_word():
     canvas.itemconfig(card_title, text="French", fill = "black")
     canvas.itemconfig(card_word, text=word_choice, fill = "black")
     canvas.itemconfig(card_face, image=img_card_front)
-    timer = window.after(3000, flip_card)
+    timer = window.after(4000, flip_card)
 
+#Function to update UI to show the English counterpart
 #-----------------------functions--------------------#
+
 def flip_card():
     global current_card
     canvas.itemconfig(card_face, image = img_card_back)
@@ -46,6 +48,7 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=eng_word, fill="white")
 
+#User Knows the French word and clicks "tick". Tick function removes word from dictionary. Triggers update_dict & pick_word
 def tick():
     global word_dict
 
@@ -54,6 +57,7 @@ def tick():
     update_dict()
     pick_word()
 
+#Fucntion to create/update a csv containing words to study.
 def update_dict():
     global word_dict, french_df
 
@@ -72,9 +76,7 @@ def update_dict():
         word_df.to_csv("words_to_learn.csv",index = False) #df to csv
 
 
-
-    # save to words_to_learn.csv
-#------------------UI-----------------#
+#------------------UI setup-----------------#
 
 window = Tk()
 window.title("Flash Card")
@@ -88,10 +90,12 @@ canvas = Canvas(width = 800, height = 526, bg = BACKGROUND_COLOR,highlightthickn
 card_face = canvas.create_image(400,265, image = img_card_front)
 canvas.grid(column = 0, row = 0, columnspan = 2)
 
+#User does not know the French word and a new word is picked.
 img_wrong = PhotoImage(file="images/wrong.png")
 button_wrong = Button(image = img_wrong, borderwidth = 0, highlightthickness = 0, command =  pick_word)
 button_wrong.grid(column=0, row = 1)
 
+#User Knows the French word and clicks "tick". Tick function removes word from dictionary
 img_right = PhotoImage(file="images/right.png")
 button_right = Button(image = img_right, borderwidth = 0, highlightthickness = 0, command =  tick)
 button_right.grid(column=1, row =1)
@@ -99,6 +103,7 @@ button_right.grid(column=1, row =1)
 card_word = canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
 card_title = canvas.create_text(400, 150, text="title", font=("Arial", 40, "italic"))
 
+#---Main loop--#
 pick_word()
 
 window.mainloop()
